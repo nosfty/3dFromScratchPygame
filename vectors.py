a1c13038db879dc3ba04abc5de10394f48c451d8
NOSFTY <unsftyghub@gmail.com>
2025-04-13 08:32:06 +0300

    Add files via upload

---
+++ vectors.py
@@ -0,0 +1,13 @@
+
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
